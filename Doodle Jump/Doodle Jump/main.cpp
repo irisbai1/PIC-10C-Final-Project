e7e12c2e63e3cde2fcbccff5a68da9c31af2cd97
[Iris Bai] <irisbai345@gmail.com>
2020-06-05 22:11:47 -0700

    Doodler now collides with platforms, responds to keyboard, and frame moves upwards

--- Doodle Jump/Doodle Jump/main.cpp
+++ Doodle Jump/Doodle Jump/main.cpp
@@ -20,6 +20,7 @@
 
 // Here is a small helper for you! Have a look.
 #include "ResourcePath.hpp"
+#include "constants.h"
 
 using namespace sf;
 
@@ -31,6 +32,8 @@ int main(int, char const**)
     srand(time(0));
     
     RenderWindow app(VideoMode(400,533), "Doodle Game!");
+    app.setFramerateLimit(80);//controls speed of doodler
+    
     Texture t1,t2,t3;
     t1.loadFromFile(resourcePath()+ "background.png");
     t2.loadFromFile(resourcePath()+ "platform.png");
@@ -40,10 +43,14 @@ int main(int, char const**)
     
     point plat[20];
     
+    // randomize platform appearance
     for (int i=0; i<10; i++) {
         plat[i].x=rand()%400;
         plat[i].y=rand()%533;
     }
+    
+    int x=100, y=100, h=200;
+    float dx = 0, dy=0;
     
     while (app.isOpen()) {
         Event e;
@@ -53,7 +60,34 @@ int main(int, char const**)
             }
         }
         
+        if(Keyboard::isKeyPressed(Keyboard::Right)) {x+=3;}
+        if(Keyboard::isKeyPressed(Keyboard::Left)) {x-=3;}
+        
+        // make Doodler jump
+        dy+= 0.2;
+        y+=dy;
+        if (y>500) {dy = -10;}
+        
+        // make Doodler bounce when it collides with platform
+        for(int i=0; i<10; i++){
+            if ((x+50>plat[i].x) && (x+20<plat[i].x+69) && (y+70>plat[i].y) && (y+70<plat[i].y+14) && (dy>0)) {dy=-10;}
+        }
+        
+        if(y<h) {
+            for (int i=0; i<10; i++) {
+                y=h;
+                plat[i].y=plat[i].y-dy;
+                if (plat[i].y>533) {
+                    plat[i].y = 0;
+                    plat[i].x = rand()%400;
+                }
+            }
+        }
+        
+        sPers.setPosition(x,y);
+        
         app.draw(sBackground);
+        sBackground.setScale(FRAME_WIDTH,FRAME_HEIGHT);
         app.draw(sPers);
         
         for (int i=0;i<10; i++) {
